a67fd52fa1a952be93fcfc7cecb2d1a4fae2c953
Sayumi <sayumi.nkmr.2019@gmail.com>
2020-04-27 10:07:36 -0700

    favicon error fixed

--- src/components/mainpage/Main.jsx
+++ src/components/mainpage/Main.jsx
@@ -5,12 +5,6 @@ import About from "./About";
 import Works from "./Works";
 import Contact from "./Contact";
 
-// const StyledDiv = styled.div`
-//   width: 86%;
-//   margin: 8%;
-//   text-align: center;
-// `;
-
 const Main = () => {
   return (
     <>
